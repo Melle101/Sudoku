a7aec0f7cdeb90e088af8ce6b1b7d7bc770f7852
Melker Stålnacke <melkerstalnacke0@gmail.com>
2021-10-25 09:30:58 +0200

    bj

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,43 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Sodoku
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    
+
     public partial class MainWindow : Window
     {
         int?[,] UserBoard = new int?[9, 9];
 
         int?[,] SolvedBoard = new int?[9, 9];
         public MainWindow()
-            {
+        {
             InitializeComponent();
             int?[,] Cells = new int?[9, 9];
             List<Algorithm> AlgorithmList = new List<Algorithm>();
             AlgorithmListBox.ItemsSource = AlgorithmList;
-            AlgorithmList.Add(new Algorithm() { AlgorithmID = "RandomInOrder"});
+            AlgorithmList.Add(new Algorithm() { AlgorithmID = "RandomInOrder" });
             AlgorithmList.Add(new Algorithm() { AlgorithmID = "InnerBoxesFirst" });
 
 
             Cells = Methods.FillArray();
 
-            /*Cell00.Text = Cells[0, 0].ToString();
+            Cell00.Text = Cells[0, 0].ToString();
             Cell01.Text = Cells[0, 1].ToString();
             Cell02.Text = Cells[0, 2].ToString();
             Cell03.Text = Cells[0, 3].ToString();
@@ -46,7 +36,7 @@ namespace Sodoku
             Cell06.Text = Cells[0, 6].ToString();
             Cell07.Text = Cells[0, 7].ToString();
             Cell08.Text = Cells[0, 8].ToString();
-            
+
             Cell05.Text = Cells[0, 5].ToString();
             Cell15.Text = Cells[1, 5].ToString();
             Cell25.Text = Cells[2, 5].ToString();
@@ -55,7 +45,7 @@ namespace Sodoku
             Cell55.Text = Cells[5, 5].ToString();
             Cell65.Text = Cells[6, 5].ToString();
             Cell75.Text = Cells[7, 5].ToString();
-            Cell85.Text = Cells[8, 5].ToString();*/
+            Cell85.Text = Cells[8, 5].ToString();
 
         }
 
@@ -70,115 +60,61 @@ namespace Sodoku
 
         private void SelectAlgorithm_Click(object sender, RoutedEventArgs e)
         {
-            if (AlgorithmListBox.SelectedIndex== -1)
+            if (AlgorithmListBox.SelectedIndex == -1)
             {
                 MessageBox.Show("Please select an algorithm first.");
             }
-            if (AlgorithmListBox.SelectedIndex== 0)
+            if (AlgorithmListBox.SelectedIndex == 0)
             {
-                
                 for (int i = 0; i < 9; i++)
                 {
                     for (int j = 0; j < 9; j++)
                     {
-                        int?[] NumbersToTest = new int?[9];
-                        
-                        for (int k = 0; k < 9; k++)
+                        int?[] arr = new int?[9];
+                        for (int k = 0; i < 9; i++)
                         {
-                            NumbersToTest[k] = new Random().Next(1, 9);
-                        }
-                        bool NumberFound = false;
-                        int l = 0;
-                        while (NumberFound == false)
-                        {
-                            SolvedBoard[i, j] = NumbersToTest[l];
-                            if (i == 0 && j == 0) Cell00.Text = SolvedBoard[0, 0].ToString();
-                            if (i == 0 && j == 1) Cell01.Text = SolvedBoard[0, 2].ToString();
-                            if (i == 0 && j == 2) Cell02.Text = SolvedBoard[0, 3].ToString();
-                            if (i == 0 && j == 3) Cell03.Text = SolvedBoard[0, 4].ToString();
-                            if (i == 0 && j == 4) Cell04.Text = SolvedBoard[0, 5].ToString();
-                            if (i == 0 && j == 5) Cell05.Text = SolvedBoard[0, 5].ToString();
-                            if (i == 0 && j == 6) Cell06.Text = SolvedBoard[0, 6].ToString();
-                            if (i == 0 && j == 7) Cell07.Text = SolvedBoard[0, 7].ToString();
-                            if (i == 0 && j == 8) Cell08.Text = SolvedBoard[0, 8].ToString();
-
-                            if (NumbersToTest[l] != SolvedBoard[i, 0] || NumbersToTest[l] != SolvedBoard[i, 1] || NumbersToTest[l] != SolvedBoard[i, 2] || NumbersToTest[l] != SolvedBoard[i, 3] || NumbersToTest[l] != SolvedBoard[i, 4] || NumbersToTest[l] != SolvedBoard[i, 5] || NumbersToTest[l] != SolvedBoard[i, 6] || NumbersToTest[l] != SolvedBoard[i, 7] || NumbersToTest[l] != SolvedBoard[i, 8])
+                            while (arr[k] == null)
                             {
-                                if (NumbersToTest[l] != SolvedBoard[0, j] || NumbersToTest[l] != SolvedBoard[1, j] || NumbersToTest[l] != SolvedBoard[2, j] || NumbersToTest[l] != SolvedBoard[3, j] || NumbersToTest[l] != SolvedBoard[4, j] || NumbersToTest[l] != SolvedBoard[5, j] || NumbersToTest[l] != SolvedBoard[6, j] || NumbersToTest[l] != SolvedBoard[7, j] || NumbersToTest[l] != SolvedBoard[8, j])
+                                int l = new Random().Next(1, 10);
+                                if ((Array.Exists(arr, element => element == k)) == false)
                                 {
-                                    if (i <= 2 && j <= 2)
-                                    {
-                                        if (NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0])
-                                        {
-                                            NumberFound = true;
-                                        }
-                                    }
-                                    if (i <= 2 && j <= 5)
-                                    {
-                                        if (NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0])
-                                        {
-                                            NumberFound = true;
-                                        }
-                                    }
-                                    if (i <= 2 && j <= 8)
-                                    {
-                                        if (NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0])
-                                        {
-                                            NumberFound = true;
-                                        }
-                                    }
-                                    if (i <= 5 && j <= 2)
-                                    {
-                                        if (NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0])
-                                        {
-                                            NumberFound = true;
-                                        }
-                                    }
-                                    if (i <= 5 && j <= 5)
-                                    {
-                                        if (NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0])
-                                        {
-                                            NumberFound = true;
-                                        }
-                                    }
-                                    if (i <= 5 && j <= 8)
-                                    {
-                                        if (NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0])
-                                        {
-                                            NumberFound = true;
-                                        }
-                                    }
-                                    if (i <= 8 && j <= 2)
-                                    {
-                                        if (NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0])
-                                        {
-                                            NumberFound = true;
-                                        }
-                                    }
-                                    if (i <= 8 && j <= 5)
-                                    {
-                                        if (NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0])
-                                        {
-                                            NumberFound = true;
-                                        }
-                                    }
-                                    if (i <= 8 && j <= 8)
-                                    {
-                                        if (NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0] || NumbersToTest[l] != SolvedBoard[0, 0])
-                                        {
-
-                                        }
-                                    }
-
-
+                                    arr[k] = l;
                                 }
-
                             }
-                            Thread.Sleep(100);
-                            l++;
                         }
+                        int?[] Row0Check = Methods.ArrayTo9Arrays(SolvedBoard).Row0;
+                        int?[] Row1Check = Methods.ArrayTo9Arrays(SolvedBoard).Row1;
+                        int?[] Row2Check = Methods.ArrayTo9Arrays(SolvedBoard).Row2;
+                        int?[] Row3Check = Methods.ArrayTo9Arrays(SolvedBoard).Row3;
+                        int?[] Row4Check = Methods.ArrayTo9Arrays(SolvedBoard).Row4;
+                        int?[] Row5Check = Methods.ArrayTo9Arrays(SolvedBoard).Row5;
+                        int?[] Row6Check = Methods.ArrayTo9Arrays(SolvedBoard).Row6;
+                        int?[] Row7Check = Methods.ArrayTo9Arrays(SolvedBoard).Row7;
+                        int?[] Row8Check = Methods.ArrayTo9Arrays(SolvedBoard).Row8;
+                        bool NumberFound = false;
+                        for (int m = 0; m < 9; m++)
+                        {
+
+                            Cell00.Text = arr[m].ToString();
+                            if (i == 0)
+                            {
+                                if ((Array.Exists(Row0Check, element => element == arr[m])) == false)
+                                {
+                                    SolvedBoard[i, j] = arr[m];
+                                    NumberFound = true;
+                                }
+                            }
+                            
+
+                            if (NumberFound)
+                            {
+                                break;
+                            }
+                        }
+
                     }
                 }
+
             }
         }
     }
--- Methods.cs
+++ Methods.cs
@@ -32,9 +32,97 @@ namespace Sodoku
             return arr;
         }
 
-        public static void FillBoard(int?[,] arr)
+        public static (int?[] Row0, int?[] Row1, int?[] Row2, int?[] Row3, int?[] Row4, int?[] Row5, int?[] Row6, int?[] Row7, int?[] Row8) ArrayTo9Arrays(int?[,] arr)
         {
-            
+            int?[] Row0 = new int?[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Row0[i] = arr[0, i];
+            }
+            int?[] Row1 = new int?[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Row0[i] = arr[1, i];
+            }
+            int?[] Row2 = new int?[9];
+            for (int i = 0; i < 0; i++)
+            {
+                Row0[i] = arr[2, i];
+            }
+            int?[] Row3 = new int?[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Row0[i] = arr[3, i];
+            }
+            int?[] Row4 = new int?[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Row0[i] = arr[4, i];
+            }
+            int?[] Row5 = new int?[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Row0[i] = arr[5, i];
+            }
+            int?[] Row6 = new int?[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Row0[i] = arr[6, i];
+            }
+            int?[] Row7 = new int?[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Row0[i] = arr[7, i];
+            }
+            int?[] Row8 = new int?[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Row0[i] = arr[8, i];
+            }
+
+            return (Row0, Row1, Row2, Row3, Row4, Row5, Row6, Row7, Row8);
+        }
+        public static int?[,] ArraysTo1Array(int?[] Row0, int?[] Row1, int?[] Row2, int?[] Row3, int?[] Row4, int?[] Row5, int?[] Row6, int?[] Row7, int?[] Row8)
+        {
+            int?[,] arr = new int?[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                arr[0, i] = Row0[i];
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                arr[1, i] = Row1[i];
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                arr[2, i] = Row2[i];
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                arr[3, i] = Row3[i];
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                arr[4, i] = Row4[i];
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                arr[5, i] = Row5[i];
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                arr[6, i] = Row6[i];
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                arr[7, i] = Row7[i];
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                arr[8, i] = Row8[i];
+            }
+
+            return arr;
         }
         
     }
